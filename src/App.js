8afce676503804dfdfa5e51e4bc64c1bbe3427eb
Umair Zafar <umairzafar164@gmail.com>
2021-06-20 01:49:33 +0500

    in process

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+class App extends React.Component {
+  state = { text: "", array: [] };
+  taskHandler = (e) => {
+    this.setState({ text: e.target.value });
+  };
+  addHandler = () => {
+    const ary = [...this.state.array, this.state.text];
+    this.setState({ array: ary });
+    this.setState({ text: "" });
+  };
+  renderedList = () => {
+    return this.state.array.map((t) => <div className='ui bulleted list'> {t}</div>);
+  };
+  render() {
+    return (
+      <div>
+        <div className="ui large icon input">
+          <label className="ui huge label"> Enter task : </label>
+          <input
+            className="ui input focus"
+            placeholder="Type here"
+            value={this.state.text}
+            onChange={this.taskHandler}
+          />
+          <button className="ui button green" onClick={this.addHandler}>
+            Add
+          </button>
+        </div>
+        <div>{this.renderedList()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
